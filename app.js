a5d1eb08f35563657a832d391d4694d751014973
Jo-hubhub <jbc66687@creighton.edu>
2022-01-27 13:39:39 -1000

    console log and loops

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+var commandLineArgs = process.argv;
+console.log(commandLineArgs);
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
-const printProfileData = (profileDataArr) => {
-    console.log(profileDataArr);
-  };
-  
+  console.log("hello node!");
+
+
+  profileDataArr.forEach((profileItem) => {
+    console.log(profileItem)
+  });
+
   printProfileData(profileDataArgs);
 
